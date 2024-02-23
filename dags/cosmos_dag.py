62e10742f38f973aefe7d34131ca0b00f62b8b5b
Ambika Garg <v-ambikagarg@microsoft.com>
2024-02-23 08:28:43 -0500

    Refactored Dag

--- dags/cosmos_dag.py
+++ dags/cosmos_dag.py
@@ -1,33 +1,42 @@
 from datetime import datetime
 import os
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
-from cosmos.profiles import PostgresUserPasswordProfileMapping
-from cosmos.constants import ExecutionMode
+from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+from pathlib import Path
 
-profile_config = ProfileConfig(
-    profile_name="default",
-    target_name="dev",
-    profile_mapping=PostgresUserPasswordProfileMapping(
-        conn_id="airflow_db",
-        profile_args={"schema": "public"},
-    ),
-)
+# dbt_project_path = Path("/Users/ambikagarg/repositories/local-cosmos-dbt/dags/dbt/cosmosproject")
 
-# execution_config = ExecutionConfig(
-#     dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
-#     execution_mode=ExecutionMode.VIRTUALENV,
+DEFAULT_DBT_ROOT_PATH = Path(__file__).parent.parent / "dags" / "jaffle_shop"
+DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
+
+# Database Profile
+# profile_config = ProfileConfig(
+#     profile_name="default",
+#     target_name="dev",
+#     profile_mapping=SnowflakeUserPasswordProfileMapping(
+#         conn_id="snowflake_default",
+#         profile_args={
+#             "database": "DEMO_DBT",
+#             "schema": "PUBLIC"
+#         },
+#     )
 # )
 
-my_cosmos_dag = DbtDag(
-    project_config=ProjectConfig(
-        "/opt/airflow/git/cosmos-dbt.git/dags/dbt",
-    ),
+profile_config = ProfileConfig(
+    profile_name="jaffle_shop",
+    target_name="fabric-dev",
+    profiles_yml_filepath=DBT_ROOT_PATH / "profiles.yml",
+)
+
+dbt_fabric_dag = DbtDag(
+    project_config=ProjectConfig(DBT_ROOT_PATH,),
+    operator_args={"install_deps": True},
     profile_config=profile_config,
-    # execution_config=execution_config,
-    # normal dag parameters
+    # execution_config=ExecutionConfig(
+    #     dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+    # ),
     schedule_interval="@daily",
-    start_date=datetime(2023, 1, 1),
+    start_date=datetime(2023, 9, 10),
     catchup=False,
-    dag_id="my_cosmos_dag",
-    default_args={"retries": 2},
+    dag_id="dbt_fabric_dag",
 )
