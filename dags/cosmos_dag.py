57f8826c4d7fb9d242cf5238e7537a9bfe8f079a
Ambika Garg <v-ambikagarg@microsoft.com>
2024-02-23 12:51:42 -0500

    Debugging dags

--- dags/cosmos_dag.py
+++ dags/cosmos_dag.py
@@ -3,22 +3,20 @@ import os
 from cosmos import DbtDag, ProjectConfig, ProfileConfig
 from pathlib import Path
 
-# DEFAULT_DBT_ROOT_PATH = Path(__file__).parent.parent / "dags" / "jaffle_shop"
+DEFAULT_DBT_ROOT_PATH = Path(__file__).parent.parent / "dags" / "jaffle_shop"
 
-DEFAULT_DBT_ROOT_PATH = "/opt/airflow/git/cosmos-dbt.git/dags/jaffle_shop"
+# DEFAULT_DBT_ROOT_PATH = "/opt/airflow/git/cosmos-dbt.git/dags/jaffle_shop"
 
 DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
 
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
     target_name="fabric-dev",
-    profiles_yml_filepath=Path("/opt/airflow/git/cosmos-dbt.git/dags/jaffle_shop/profiles.yml"),
-    # profiles_yml_filepath=DBT_ROOT_PATH / "profiles.yml",
+    profiles_yml_filepath=DBT_ROOT_PATH / "profiles.yml",
 )
 
 dbt_fabric_dag = DbtDag(
-    # project_config=ProjectConfig(DBT_ROOT_PATH,),
-    project_config=ProjectConfig("/opt/airflow/git/cosmos-dbt.git/dags/jaffle_shop",),
+    project_config=ProjectConfig(DBT_ROOT_PATH,),
     operator_args={"install_deps": True},
     profile_config=profile_config,
     schedule_interval="@daily",
