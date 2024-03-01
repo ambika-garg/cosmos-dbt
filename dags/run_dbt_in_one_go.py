5f13dbce73a5740f3bfcd953dc20ac790998a062
Ambika Garg <v-ambikagarg@microsoft.com>
2024-03-01 09:39:40 -0500

    Run DBT in one go - Debuggging

--- dags/run_dbt_in_one_go.py
+++ dags/run_dbt_in_one_go.py
@@ -14,16 +14,24 @@ DEFAULT_DBT_ROOT_PATH = Path(__file__).parent.parent / "dags" / "jaffle_shop"
     dag_id="Run_DBT_in_one_go",
 )
 def simple_dbt_dag():
+
+    # && dbt run --models
+
     dbt_run = BashOperator(
         task_id="dbt_run",
         # bash_command="source $PATH_TO_DBT_VENV && dbt run --models .",
-        bash_command="cd $DEFAULT_DBT_ROOT_PATH && dbt run --models",
+        bash_command="cd $DEFAULT_DBT_ROOT_PATH && pwd",
         env={"DEFAULT_DBT_ROOT_PATH": str(DEFAULT_DBT_ROOT_PATH)},
         # env={"PATH_TO_DBT_VENV": PATH_TO_DBT_VENV},
         cwd=DEFAULT_DBT_ROOT_PATH,
     )
 
-    dbt_run
+    print_pwd = BashOperator(
+        task_id="print_pwd",
+        bash_command="pwd",
+    )
+
+    dbt_run >> print_pwd
 
 
 simple_dbt_dag()
