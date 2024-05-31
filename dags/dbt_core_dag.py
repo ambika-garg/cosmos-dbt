ba3ca17151034e7f0c894e05486b5efde708dcd0
ambika-garg <70703123+ambika-garg@users.noreply.github.com>
2024-05-31 15:57:18 -0400

    Update dbt_core_dag.py

--- dags/dbt_core_dag.py
+++ dags/dbt_core_dag.py
@@ -27,7 +27,7 @@ with DAG(
 
     print_dbt_folder = BashOperator(
       task_id='print_dbt_folder',
-      bash_command='ls'
+      bash_command=f'cd {DBT_ROOT_PATH}'
     )
     
     dbt_debug = BashOperator(
